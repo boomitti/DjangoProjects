453d91a307174dab57aabf4d85f6e4a2b15aa7b0
Boom Ittiprateep <sahatrat.itti@gmail.com>
2023-12-13 15:47:32 +0700

    Update project with CRUD and bootstrap modal

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('about', views.about),
     path('form', views.form),
+    path('edit/<person_id>', views.edit),
+    path('delete/<person_id>', views.delete)
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from myapp.models import Person
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,42 @@ def about(request):
     return render(request, "about.html")
 
 def form(request):
-    return render(request, "form.html")
+    if request.method == "POST":
+        # receive data
+        form_name = request.POST["name"]
+        form_age = request.POST["age"]
+
+        # save data
+        person = Person.objects.create(
+            name = form_name,
+            age = form_age
+        )
+        person.save()
+        messages.success(request, "Data registered")
+
+        # redirect
+        return redirect("/")
+    else : 
+        return render(request, "form.html")
+
+def edit(request, person_id):
+    person = Person.objects.get(id=person_id)
+
+    if request.method == "POST":
+        person.name = request.POST["name"]
+        person.age = request.POST["age"]
+        person.save()
+        messages.success(request, "person updated")
+        return redirect("/")
+    else:
+        # get person info from DB
+        return render(request, "edit.html", {
+            "person": person
+        })
+
+def delete(request, person_id):
+    person = Person.objects.get(id=person_id)
+    
+    person.delete()
+    messages.success(request, "person deleted")
+    return redirect("/")
